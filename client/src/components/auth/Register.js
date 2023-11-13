baa4fd10cda5f5972b068fec74f2afa4c0351908
gnanamr18 <gnanam.r18@gmail.com>
2023-11-14 01:52:44 +0530

    first commit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 // import { json } from "express";
 import React, { Fragment, useState } from "react";
 import {Link} from 'react-router-dom'
